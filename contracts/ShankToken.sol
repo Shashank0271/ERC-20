65142aec2cd809d3fd4e0400b73b0bb0603d4d29
Shashank <ssingh0271@gmail.com>
2022-12-23 16:40:16 +0530

    added tests for transfer from function

--- contracts/ShankToken.sol
+++ contracts/ShankToken.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 import "hardhat/console.sol";
+
 contract ShankToken {
-    string public name = "ShankToken" ;
-    string public symbol = "STK" ;
-    string public standard = "Shank Token v1.0" ;
+    string public name = "ShankToken";
+    string public symbol = "STK";
+    string public standard = "Shank Token v1.0";
     uint public totalSupply;
     mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint))  private _allowed;
+    mapping(address => mapping(address => uint)) private _allowed;
 
     event Transfer(address _from, address _to, uint256 _value);
     event Approval(address _owner, address _spender, uint256 _value);
-    
+
     constructor(uint _totalSupply) {
         _balances[msg.sender] = _totalSupply;
         totalSupply = _totalSupply;
@@ -32,29 +33,52 @@ contract ShankToken {
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
-    
-  /**
-   * @dev Function to check the amount of tokens that an owner allowed to a spender.
-   * @param owner address The address which owns the funds.
-   * @param spender address The address which will spend the funds.
-   * @return A uint256 specifying the amount of tokens still available for the spender.
-   */
-  function allowance(
-    address owner,
-    address spender
-   )
-    public
-    view
-    returns (uint256)
-  {
-    return _allowed[owner][spender];
-  }
 
-    function approve(address _spender, uint256 _value) public returns (bool){
-        require(balanceOf(_spender) >= _value , 'account has insufficient tokens');
-        _allowed[msg.sender][_spender] = _value ;
-        emit Approval(msg.sender , _spender , _value) ;
-        return true ;
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param owner address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(
+        address owner,
+        address spender
+    ) public view returns (uint256) {
+        return _allowed[owner][spender];
     }
 
+    //@dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        _allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /*
+        this is to allow contracts to transfer tokens on your (owners) behalf
+        there are three accounts in this case : 
+        the one we are calling from
+        the one from where tokens are transferred
+        the one to which tokens are transferred.
+        This is a delegated transfer .
+    */
+    function transferFrom(
+        address _from,
+        address _to,
+        uint _amount
+    ) public returns (bool) {
+        require(
+            balanceOf(_from) >= _amount,
+            "account does not have enough tokens to transfer"
+        );
+        require(
+            allowance(msg.sender, _from) >= _amount,
+            "account does not have permission to transfer all tokens"
+        );
+        _balances[_from] -= _amount ;
+        _balances[_to] += _amount ;
+        _allowed[msg.sender][_from] -= _amount ;
+        emit Transfer(_from , _to , _amount) ;
+        return true;
+    }
 }
--- test/ShankToken.js
+++ test/ShankToken.js
@@ -3,11 +3,11 @@ const { expect } = require('chai') ;
 
 describe('ShankToken Contract' , ()=>{
     let tokenContract ;
-    let owner , acc1 ;
+    let owner , acc1 , acc2 ;
     beforeEach(async()=>{
         Token = await ethers.getContractFactory('ShankToken');
         tokenContract = await Token.deploy(350) ;
-        [owner , acc1] = await ethers.getSigners() ;
+        [owner , acc1 , acc2] = await ethers.getSigners() ;
     });
     describe('Deployment' , ()=>{
         it('allocates the initial supply on deployment' , async()=>{
@@ -35,14 +35,38 @@ describe('ShankToken Contract' , ()=>{
         beforeEach(async()=>{
             await expect(tokenContract.transfer(acc1.address , 250)).to.not.be.reverted ;
         });
-        it('checks for approval to finish' , async()=>{
+        it('should emit Approval event after transaction is completed successfully' , async()=>{
+            await expect(tokenContract.approve(acc1.address , 100)).to.emit(tokenContract , 'Approval').withArgs(owner.address , acc1.address , 100) ;
+        });
+        it('checks the amount the owner has allowed to a spender' , async()=>{
             await expect(tokenContract.approve(acc1.address , 100)).to.not.be.reverted ;
             transaction = await tokenContract.allowance(owner.address , acc1.address) ;
             expect(transaction).to.not.be.undefined ;
             expect(transaction).to.be.equals(100);
         });
-        it('should emit Approval event after transaction is completed successfully' , async()=>{
-            await expect(tokenContract.approve(acc1.address , 100)).to.emit(tokenContract , 'Approval').withArgs(owner.address , acc1.address , 100) ;
+    });
+    describe('Transfer From' , ()=>{
+        beforeEach(async()=>{
+            //funding the account with 200 tokens
+            await tokenContract.transfer(acc1.address , 200) ;
+        }) ;
+        it('must revert if account does not have enough tokens , but has permission to transfer tokens' , async()=>{
+            await expect(tokenContract.approve(acc1.address , 200)).to.emit(tokenContract , 'Approval').withArgs(owner.address , acc1.address , 200) ;
+            await expect(tokenContract.transferFrom(acc1.address , acc2.address , 9000)).to.be.revertedWith('account does not have enough tokens to transfer') ;
+        });
+        it('must revert if account does not have permission to transfer tokens'  , async()=>{
+            await expect(tokenContract.transferFrom(acc1.address , acc2.address , 20)).to.be.revertedWith('account does not have permission to transfer all tokens') ;
+        });
+        it('must transfer tokens when all conditions are met' , async()=>{
+            //allowed to transfer 100 tokens (owns 200 initially)
+            await tokenContract.approve(acc1.address , 100) ;
+            //transfers 10 tokens
+            await expect(tokenContract.transferFrom(acc1.address , acc2.address , 10)).to.not.be.reverted ;
+            expect(await tokenContract.balanceOf(acc1.address)).to.be.equals(190) ;
+            expect(await tokenContract.balanceOf(acc2.address)).to.be.equals(10) ;
+            //now allowed to transfer 100-10=90 tokens
+            expect(await tokenContract.allowance(owner.address , acc1.address)).to.be.equals(90) ;
         });
     });
+
 })
