685bd002bb27e68543aedfdedf417fbcf34cd7dc
Shashank <ssingh0271@gmail.com>
2022-12-23 15:54:29 +0530

    added tests for the approve function

--- contracts/ShankToken.sol
+++ contracts/ShankToken.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
-
+import "hardhat/console.sol";
 contract ShankToken {
+    string public name = "ShankToken" ;
+    string public symbol = "STK" ;
+    string public standard = "Shank Token v1.0" ;
     uint public totalSupply;
     mapping(address => uint256) private _balances;
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    mapping(address => mapping(address => uint))  private _allowed;
 
+    event Transfer(address _from, address _to, uint256 _value);
+    event Approval(address _owner, address _spender, uint256 _value);
+    
     constructor(uint _totalSupply) {
         _balances[msg.sender] = _totalSupply;
         totalSupply = _totalSupply;
@@ -26,4 +32,29 @@ contract ShankToken {
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
+    
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param owner address The address which owns the funds.
+   * @param spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(
+    address owner,
+    address spender
+   )
+    public
+    view
+    returns (uint256)
+  {
+    return _allowed[owner][spender];
+  }
+
+    function approve(address _spender, uint256 _value) public returns (bool){
+        require(balanceOf(_spender) >= _value , 'account has insufficient tokens');
+        _allowed[msg.sender][_spender] = _value ;
+        emit Approval(msg.sender , _spender , _value) ;
+        return true ;
+    }
+
 }
--- test/ShankToken.js
+++ test/ShankToken.js
@@ -25,9 +25,24 @@ describe('ShankToken Contract' , ()=>{
         it('transfers money from sender to an address' , async()=>{
             await expect(tokenContract.transfer(acc1.address , 20)).to.not.be.reverted ;
             expect(await tokenContract.balanceOf(acc1.address)).to.be.equals(20) ;
+            expect(await tokenContract.balanceOf(owner.address)).to.be.equals(330) ;
         });
         it('should not transfer when sender has insufficient balance' , async()=>{
             await expect(tokenContract.transfer(acc1.address , 600)).to.be.revertedWith('sender has insufficient tokens');
+        });
+    });
+    describe('Approve' , ()=>{
+        beforeEach(async()=>{
+            await expect(tokenContract.transfer(acc1.address , 250)).to.not.be.reverted ;
+        });
+        it('checks for approval to finish' , async()=>{
+            await expect(tokenContract.approve(acc1.address , 100)).to.not.be.reverted ;
+            transaction = await tokenContract.allowance(owner.address , acc1.address) ;
+            expect(transaction).to.not.be.undefined ;
+            expect(transaction).to.be.equals(100);
+        });
+        it('should emit Approval event after transaction is completed successfully' , async()=>{
+            await expect(tokenContract.approve(acc1.address , 100)).to.emit(tokenContract , 'Approval').withArgs(owner.address , acc1.address , 100) ;
         });
     });
 })
