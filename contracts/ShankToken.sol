bb579fd28f3fca67b9e8c86e88e1cdd3f0f7d9e2
Shashank <ssingh0271@gmail.com>
2022-12-26 10:54:18 +0530

    added safemath via npm dependency

--- contracts/ShankToken.sol
+++ contracts/ShankToken.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 import "hardhat/console.sol";
-
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract ShankToken {
+    using SafeMath for uint256 ;
     string public name = "ShankToken";
     string public symbol = "STK";
     string public standard = "Shank Token v1.0";
-    uint public totalSupply;
+    uint256 public totalSupply;
     mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint)) private _allowed;
+    mapping(address => mapping(address => uint256)) private _allowed;
 
     event Transfer(address _from, address _to, uint256 _value);
     event Approval(address _owner, address _spender, uint256 _value);
 
-    constructor(uint _totalSupply) {
+    constructor(uint256 _totalSupply) {
         _balances[msg.sender] = _totalSupply;
         totalSupply = _totalSupply;
     }
@@ -28,8 +29,8 @@ contract ShankToken {
             balanceOf(msg.sender) >= _value,
             "sender has insufficient tokens"
         );
-        _balances[msg.sender] -= _value;
-        _balances[_to] += _value;
+        _balances[msg.sender] = _balances[msg.sender].sub(_value);
+        _balances[_to] = _balances[_to].add(_value);
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
@@ -65,7 +66,7 @@ contract ShankToken {
     function transferFrom(
         address _from,
         address _to,
-        uint _amount
+        uint256 _amount
     ) public returns (bool) {
         require(
             balanceOf(_from) >= _amount,
@@ -75,31 +76,31 @@ contract ShankToken {
             allowance(msg.sender, _from) >= _amount,
             "cannot transfer value larger than approved amount"
         );
-        _balances[_from] -= _amount;
-        _balances[_to] += _amount;
-        _allowed[msg.sender][_from] -= _amount;
+        _balances[_from] = _balances[_from].sub(_amount);
+        _balances[_to] = _balances[_to].add(_amount);
+        _allowed[msg.sender][_from]  = _allowed[msg.sender][_from].sub(_amount);
         emit Transfer(_from, _to, _amount);
         return true;
     }
 
     function increaseAllowance(
         address accountAdd,
-        uint incrementValue
+        uint256 incrementValue
     ) public returns (bool) {
-        _allowed[msg.sender][accountAdd] += incrementValue;
+        _allowed[msg.sender][accountAdd] = _allowed[msg.sender][accountAdd].add(incrementValue);
         emit Approval(msg.sender, accountAdd, _allowed[msg.sender][accountAdd]);
         return true;
     }
 
     function decreaseAllowance(
         address account,
-        uint decrementBy
+        uint256 decrementBy
     ) public returns (bool) {
         require(
             _allowed[msg.sender][account] >= decrementBy,
             "allowed amount must be greater than or equal to decrement"
         );
-        _allowed[msg.sender][account] -= decrementBy;
+        _allowed[msg.sender][account] = _allowed[msg.sender][account].sub(decrementBy);
         emit Approval(msg.sender, account, _allowed[msg.sender][account]);
         return true ;
     }
